7f01dafcbb2ea32ea94d809c55a44d75619f66ff
Guilherme <guicolares@gmail.com>
2020-05-29 17:11:08 -0300

    bug fix navigation for large title

--- wooptest/Supporting Files/AppDelegate.swift
+++ wooptest/Supporting Files/AppDelegate.swift
@@ -14,11 +14,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        setNavigationBarColor()
         initMainScreen()
         return true
     }
     
-    func initMainScreen() {
+    private func setNavigationBarColor() {
+        let mainColor = UIColor(red: 23/255, green: 230/255, blue: 119/255, alpha: 1)
+        
+        if #available(iOS 13.0, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.backgroundColor = mainColor
+            appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+
+            UINavigationBar.appearance().tintColor = .white
+            UINavigationBar.appearance().standardAppearance = appearance
+            UINavigationBar.appearance().compactAppearance = appearance
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        } else {
+            UINavigationBar.appearance().tintColor = .white
+            UINavigationBar.appearance().barTintColor = mainColor
+            UINavigationBar.appearance().isTranslucent = false
+        }
+    }
+    
+    private func initMainScreen() {
         let vc = WPEventsRouter().build()
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.rootViewController = vc
